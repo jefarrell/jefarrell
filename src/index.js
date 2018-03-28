6d6c0eae058f61fbc2efb739ded1c9974663f2fe
John Farrell <johnefarrell18@gmail.com>
2018-03-27 23:06:11 -0700

    scrolling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import noise from './noise'
 require('./styles/main.scss')
 
+const svgDir = document.getElementById('svg-container'),
+      svgns = 'http://www.w3.org/2000/svg',
+      height = document.body.clientHeight,
+      width = document.body.clientWidth;
+
 
 const noiseGen = iter => {
 	const multiplier = 500;
@@ -9,87 +14,45 @@ const noiseGen = iter => {
 }
 
 
-
-
-const gradient = (url, off1, off2) => {
-	let grad = document.createElementNS(url, 'linearGradient');
-	grad.setAttribute('class', 'root-rect-gradient');
-	grad.setAttribute('x1', '0%');
-	grad.setAttribute('x2', '50%');
-	grad.setAttribute('y1', '0%');
-	grad.setAttribute('y2', '100%');
-	grad.setAttribute('offset', "50%");
-	grad.setAttribute('stop-color', "red");
-	grad.setAttribute('offset', "51%");
-	grad.setAttribute('stop-color', "#FFF");
-
-
-	var stops = [
-	    {
-	        "color": "#ff0000",
-	        "offset": "50%"
-	    },{
-	        "color": "#fff",
-	        "offset": "51%"
-	    }
-	];
-
-	// Parses an array of stop information and appends <stop> elements to the <linearGradient>
-	for (var i = 0, length = stops.length; i < length; i++) {
-
-	    // Create a <stop> element and set its offset based on the position of the for loop.
-	    var stop = document.createElementNS(url, 'stop');
-	    stop.setAttribute('offset', stops[i].offset);
-	    stop.setAttribute('stop-color', stops[i].color);
-
-	    // Add the stop to the <lineargradient> element.
-	    grad.appendChild(stop);
-
-	}
-
-
-
-
-	return grad;
+const makeRect = ( className, iter, color) => {
+  let rect = document.createElementNS(svgns, 'rect');
+  rect.setAttribute('class', className);
+  rect.setAttributeNS(null, 'y', iter * 50);
+  rect.setAttributeNS(null, 'height', iter * 2 );
+  rect.setAttributeNS(null, 'width', width-100 );
+  rect.setAttributeNS(null, 'fill', color);
+  return rect;
 }
 
 
 const init = () => {
-
-  	const height = document.body.clientHeight;
-  	const width = document.body.clientWidth;
-  	const noiseVars = [];
-
+  const noiseVars = [];
 	for (let j = 10; j < height; j += 5) {
 		noiseVars.push( noiseGen(j) );
 	}
 
-	const svgDir = document.getElementById('svg-container');
-	const svgns = 'http://www.w3.org/2000/svg';
+	for (let i = 0; i < noiseVars.length; i++) {
+    const r1 = makeRect('root-rect', i, '#0000FF');
+    const r2 = makeRect('top-rect', i, '#FF0000');
+    svgDir.appendChild(r1);
+    svgDir.appendChild(r2);
+	}
+  
+  const rects = document.getElementsByClassName('top-rect');
+  for (let r of rects ) {   
+    r.setAttributeNS(null, 'width', Math.abs(r.getBoundingClientRect().top));
+  }
+}
 
 
-
-
-
-  	for (let i = 0; i < noiseVars.length; i++) {
-  		
-  		let rect = document.createElementNS(svgns, 'rect');
-  		let defs = document.createElementNS(svgns, 'defs');
-  		let blah = gradient(svgns, 1, 1);
-  		defs.appendChild(blah);
-  		rect.appendChild(defs);
-  		rect.setAttribute('class', 'root-rect');
-  		rect.setAttributeNS(null, 'x', width * 0.05);
-  		rect.setAttributeNS(null, 'y', i * 50);
-  		rect.setAttributeNS(null, 'height', i * 2.5);
-  		rect.setAttributeNS(null, 'width', '90%');
-  		rect.setAttribute(null, 'fill', 'url(.root-rect-gradient)');
-  		//rect.setAttributeNS(null, 'fill', 'rgba(0, 0, 255, 1')
-  		svgDir.appendChild(rect);
-
-  	}
- }
-
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('scroll', e => {
+    const rects = document.getElementsByClassName('top-rect');
+    for (let r of rects ) {   
+      r.setAttributeNS(null, 'width', Math.abs(r.getBoundingClientRect().top));
+    }
+  })
+})
 
 
 init();
