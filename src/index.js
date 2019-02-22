5d7b36762c3b684e9bf8b419c0722fe98ae9365a
John Farrell <johnefarrell18@gmail.com>
2019-02-21 18:20:16 -0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,103 +1,17 @@
-import noise from './noise'
 require('./styles/main.scss')
 
-const svgDir = document.getElementById('svg-container')
-const svgns = 'http://www.w3.org/2000/svg'
-let height = document.body.clientHeight
-let width = document.body.clientWidth
-let iterX = 0;
-
-const noiseGen = iter => {
-  const multiplier = 500;
-  const subt = 400;
-  return Math.abs(iter - (noise(0, iter / 300) * multiplier) - subt)
+function genDivs(v){ 
+  var e = document.body; // whatever you want to append the rows to: 
+  for(var i = 0; i < v; i++){ 
+    var row = document.createElement("div"); 
+    row.className = "row"; 
+    for(var x = 0; x <= v; x++){ 
+        var cell = document.createElement("div"); 
+        cell.className = "row-item"; 
+        row.appendChild(cell); 
+    } 
+    e.appendChild(row); 
+  } 
 }
 
-
-const makeRect = (className, iter, noise, color) => { 
-  const rect = document.createElementNS(svgns, 'rect')
-  rect.setAttribute('class', className)
-  rect.setAttributeNS(null, 'y', iter * 50)
-  rect.setAttributeNS(null, 'height', Math.abs(noise / 3))
-  rect.setAttributeNS(null, 'fill', color)
-  return rect;
-}
-
-
-const init = () => {
-  return new Promise( (resolve, reject) => {
-    
-    const noiseVars = []
-  	for (let j = 10; j < height; j += 5) {
-  		noiseVars.push( noiseGen(j) )
-  	}
-    
-    resolve(noiseVars)
-
-  })
-}
-
-
-const createRects = arr => {
-  return new Promise ( (resolve, reject) => {
-    for (let i = 0; i < arr.length; i++) {
-      iterX += 0.01;
-      let n = 255 - arr[i] * iterX
-      const r1 = makeRect('root-rect', i, n, `rgba(0,0,255,${iterX*2}`)
-      const r2 = makeRect('top-rect', i, n, `rgba(255,0,0,${iterX*3}`)
-      svgDir.appendChild(r1)
-      svgDir.appendChild(r2)
-    }
-    resolve()
-  })
-}
-
-
-const setRectWidths = () => {
-  return new Promise( (resolve,reject) => {
-    const topRects = document.getElementsByClassName('top-rect')
-    const rootRects = document.getElementsByClassName('root-rect')
-
-    for (let rr of rootRects ) {   
-      rr.setAttributeNS(null, 'width', width)
-    }
-    for (let tr of topRects ) { 
-      tr.setAttributeNS(null, 'width', Math.abs(tr.getBoundingClientRect().top*2))
-    }
-
-    resolve()
-  })
-}
-
-
-const asyncWrap = async () => {
-  try {
-    const makeNoise = await init()
-    const rectGroup = await createRects(makeNoise)
-    await setRectWidths(rectGroup)
-  }
-  catch (err) {
-    throw err
-  }
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('root').setAttribute('style', `border: ${width*0.02}px solid #000000`)
-  
-  asyncWrap()
-  
-  window.addEventListener('scroll', e => {
-    const rects = document.getElementsByClassName('top-rect')
-    for (let r of rects ) {   
-      r.setAttributeNS(null, 'width', Math.abs(r.getBoundingClientRect().top*2))
-    }
-  })
-
-  window.addEventListener('resize', () => { 
-    height = document.body.clientHeight
-    width = document.body.clientWidth
-    setRectWidths()
-  })
-})
-
+genDivs(7)
