349aa79d9c6e6923c8458d50e4b4d2f271c57666
John Farrell <johnefarrell18@gmail.com>
2020-07-20 22:31:30 -0700

    delete it all

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
-import * as d3 from "d3";
-require("./styles/main.scss");
+"use strict";
+
+var d3 = _interopRequireWildcard(require("d3"));
+
+function _getRequireWildcardCache() {
+  if (typeof WeakMap !== "function") return null;
+  var cache = new WeakMap();
+  _getRequireWildcardCache = function() {
+    return cache;
+  };
+  return cache;
+}
+
+function _interopRequireWildcard(obj) {
+  if (obj && obj.__esModule) {
+    return obj;
+  }
+  if (obj === null || (typeof obj !== "object" && typeof obj !== "function")) {
+    return { default: obj };
+  }
+  var cache = _getRequireWildcardCache();
+  if (cache && cache.has(obj)) {
+    return cache.get(obj);
+  }
+  var newObj = {};
+  var hasPropertyDescriptor =
+    Object.defineProperty && Object.getOwnPropertyDescriptor;
+  for (var key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      var desc = hasPropertyDescriptor
+        ? Object.getOwnPropertyDescriptor(obj, key)
+        : null;
+      if (desc && (desc.get || desc.set)) {
+        Object.defineProperty(newObj, key, desc);
+      } else {
+        newObj[key] = obj[key];
+      }
+    }
+  }
+  newObj.default = obj;
+  if (cache) {
+    cache.set(obj, newObj);
+  }
+  return newObj;
+}
 
 /*
  * Multi-line waveform animation in D3
@@ -8,6 +51,7 @@ require("./styles/main.scss");
  */
 const param = 48;
 const arr = [];
+
 for (let i = 0; i < 24; i++) arr.push(1);
 
 const svg = d3.select("svg");
@@ -15,7 +59,6 @@ const width = window.innerWidth;
 const height = +svg.attr("height");
 const x = d3.scaleLinear().range([0, width]);
 const angles = d3.range(0, 2 * Math.PI, Math.PI / 10);
-
 const path = svg
   .append("g")
   .attr("transform", `translate(0, ${height / 2})`)
@@ -43,7 +86,6 @@ const path = svg
         );
       });
   });
-
 d3.timer(() => {
   path.attr("d", d => {
     return d(angles);
